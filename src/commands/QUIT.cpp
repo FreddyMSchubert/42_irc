c32e171c9f45149e72f8be72084e537404d57636
FreddyMSchubert <github@frederickschubert.de>
2025-01-15 10:57:39 +0100

    safer client disconnecting, expect its just sad at this point

--- src/commands/QUIT.cpp
+++ src/commands/QUIT.cpp
@@ -2,6 +2,7 @@
 
 std::string CommandHandler::HandleQUIT(const std::vector<std::string> &parts, Client & client, Server &server)
 {
+	(void)server;
 	if (parts.size() > 1)
 		return "QUIT :" + parts[1] + "\r\n";
 	client.shouldDisconnect = true;
