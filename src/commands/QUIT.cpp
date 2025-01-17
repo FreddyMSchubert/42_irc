91613750d8569a687037601c033768fa8429e84f
Z1nnlos <Noel.Sabia@gmx.net>
2025-01-17 10:59:07 +0100

    merge conflicts

--- src/commands/QUIT.cpp
+++ src/commands/QUIT.cpp
@@ -2,12 +2,8 @@
 
 void CommandHandler::HandleQUIT(const std::vector<std::string> &parts, Client & client, Server &server)
 {
-<<<<<<< HEAD
 	(void)server;
 	(void)parts;
-=======
-	(void) parts;
->>>>>>> 353aea68b791cf6639b8b8bf5af629c79949d725
 	client.shouldDisconnect = true;
 	Channel *channel = nullptr;
 	if (client.channelId)
